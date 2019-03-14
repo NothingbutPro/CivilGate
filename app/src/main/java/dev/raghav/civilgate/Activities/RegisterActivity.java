ddaa19a33fda6ed40fb9e38f96ad4727d72e7505
NothingbutPro <paragsharma611@gmail.com>
2019-03-14 10:58:49 +0530

    by parag

--- app/src/main/java/dev/raghav/civilgate/Activities/RegisterActivity.java
+++ app/src/main/java/dev/raghav/civilgate/Activities/RegisterActivity.java
@@ -7,26 +7,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import dev.raghav.civilgate.R;
 
 public class RegisterActivity extends AppCompatActivity {
      EditText email , password , passing_year , ful_name , mobile,address ;
      Spinner type;
+     int a;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
-
+//         a=10;
+//        Toast.makeText(this, "a value at oncreate is"+a, Toast.LENGTH_SHORT).show();
         getSupportActionBar().hide();
-        init();
+//        init(a);
+
+//        Log.d("init" , "workssdfsdf a"+a);
 //        Intent intent  = new Intent(RegisterActivity.this , Main2Activity.class);
 //        startActivity(intent);
 
     }
 
-    private void init() {
-        Log.d("init" , "works");
+    private void init(int a) {
+//        this.a = a+10;
+//        Log.d("init" , "works a"+a);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         passing_year = findViewById(R.id.passout_year);
